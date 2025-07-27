fd5e5b4d5310e7f8f0e58cd23d1e9df5492d81be
Workinew Gebrie <gworkinew@gmail.com>
2025-07-27 19:33:29 +0300

    Refactor: Replaced prop drilling with Context API

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#222', color: 'white', 
+    textAlign: 'center', padding: '10px' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#f0f0f0', padding: '15px', 
+    fontSize: '18px', textAlign: 'center' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
